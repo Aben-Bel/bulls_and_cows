978627030470ccd41619902c378ce0e390bd0076
aben-bel <abenezer.sbelay@gmail.com>
2019-09-08 12:34:46 +0300

    implement sending guess

--- connect/connect.js
+++ connect/connect.js
@@ -7,11 +7,90 @@ const playInit = document.querySelector('#playCreate');
 const tokenBox = document.querySelector('#tokenBox');
 const tokenValue = document.querySelector('#tokenValue');
 const sendMessage = document.querySelector('#sendMessage');
+const tbody = document.querySelector('#tbody');
+const sendGuess = document.querySelector('#submit-guess');
 
 let selfIdInit;
 let joinId;
 let peerInit; // simple-peer initiator
 let peerJoin; // simple-peer join
+
+const appendIandP = (userType, guess, i, p) => {
+  const lastRow = tbody.rows[tbody.children.length - 1];
+
+  if (userType === 'self' && lastRow.cells[0].textContent === '') {
+    lastRow.cells[0].textContent = guess;
+    lastRow.cells[1].textContent = i;
+    lastRow.cells[2].textContent = p;
+  } else if (userType === 'opponent' && lastRow.cells[3].textContent === '') {
+    lastRow.cells[3].textContent = guess;
+    lastRow.cells[4].textContent = i;
+    lastRow.cells[5].textContent = p;
+  } else if (userType === 'self' && lastRow.cells[3].textContent !== '' && lastRow.cells[0].textContent !== '') {
+    const tr = document.createElement('TR');
+    const td1 = document.createElement('TD');
+    td1.textContent = guess;
+    tr.appendChild(td1);
+
+    const td2 = document.createElement('TD');
+    td2.textContent = i;
+    tr.appendChild(td2);
+
+    const td3 = document.createElement('TD');
+    td3.textContent = p;
+    tr.appendChild(td3);
+
+    const td4 = document.createElement('TD');
+    td4.textContent = '';
+    tr.appendChild(td4);
+
+    const td5 = document.createElement('TD');
+    td5.textContent = '';
+    tr.appendChild(td5);
+
+    const td6 = document.createElement('TD');
+    td6.textContent = '';
+    tr.appendChild(td6);
+
+    tbody.appendChild(tr);
+  } else if (userType === 'opponent' && lastRow.cells[3].textContent !== '' && lastRow.cells[0].textContent !== '') {
+    const tr = document.createElement('TR');
+
+    const td1 = document.createElement('TD');
+    td1.textContent = '';
+    tr.appendChild(td1);
+
+    const td2 = document.createElement('TD');
+    td2.textContent = '';
+    tr.appendChild(td2);
+
+    const td3 = document.createElement('TD');
+    td3.textContent = '';
+    tr.appendChild(td3);
+
+    const td4 = document.createElement('TD');
+    td4.textContent = guess;
+    tr.appendChild(td4);
+
+    const td5 = document.createElement('TD');
+    td5.textContent = i;
+    tr.appendChild(td5);
+
+    const td6 = document.createElement('TD');
+    td6.textContent = p;
+    tr.appendChild(td6);
+
+    tbody.appendChild(tr);
+  } else {
+    console.log('Illegal append request by ', userType);
+  }
+};
+
+const isTurn = () => {
+  const lastRow = tbody.rows[tbody.children.length - 1];
+  return (lastRow.cells[3].textContent === '' && lastRow.cells[0].textContent === '') || lastRow.cells[3].textContent !== '';
+};
+
 
 const appendMessage = (userType, message) => {
   const messageBox = document.querySelector('.message-box');
@@ -39,7 +118,12 @@ playInit.addEventListener('click', () => {
   });
   // listening for message from other peer
   peerInit.on('data', (data) => {
-    appendMessage('oppo', data);
+    console.log('message in init: ', data);
+    if (/(^;[0-9]{4};)$/.test(data)) {
+      appendIandP('opponent', data.split(';')[1], 0, 0);
+    } else {
+      appendMessage('opponent', data);
+    }
   });
 });
 
@@ -67,7 +151,12 @@ playToken.addEventListener('click', () => {
   });
   // listening for message from other peer
   peerJoin.on('data', (data) => {
-    appendMessage('self', data);
+    console.log('message in join: ', data);
+    if (/(^;[0-9]{4};)$/.test(data)) {
+      appendIandP('opponent', data.split(';')[1], 0, 0);
+    } else {
+      appendMessage('opponent', data);
+    }
   });
 });
 
@@ -84,6 +173,21 @@ sendMessage.addEventListener('click', () => {
     appendMessage('oppo', message);
   }
 });
+
+sendGuess.addEventListener('click', () => {
+  const guess = document.querySelector('#submit-input-text').value;
+  if (isTurn()) {
+    appendIandP('self', guess, 0, 0);
+    if (selfIdInit) {
+      peerInit.send(`;${guess};`);
+    } else {
+      peerJoin.send(`;${guess};`);
+    }
+  } else {
+    console.log('invalid request, cannot send guess');
+  }
+});
+
 
 // when join id sends its id to server,
 // the server sends the message to everyone.
